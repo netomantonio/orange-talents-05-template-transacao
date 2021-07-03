615899de20490ccdbd43bc4993ef4e6e3e8ca818
[Neto] Antonio Miranda <netomantonio@gmail.com>
2021-07-03 17:42:43 -0300

    autenticacao e autorização

---
+++ src/main/java/br/com/zupacademy/transacoes/transacoes/security/SecurityConfiguration.java
@@ -0,0 +1,21 @@
+package br.com.zupacademy.transacoes.transacoes.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests( authorizeRequest ->
+                authorizeRequest
+
+                        .antMatchers(HttpMethod.GET, "/api/cartoes/transacoes**").hasAuthority("SCOPE_transacao:read")
+                        .anyRequest().authenticated()
+        ).oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+    }
+}
